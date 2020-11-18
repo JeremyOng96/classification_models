7bacde268853257dc64f8ab1712e21da11c6e123
JeremyOng96 <jeremyunderscore@gmail.com>
2020-11-18 15:56:43 +0800

    Update models_factory.py

--- classification_models/models_factory.py
+++ classification_models/models_factory.py
@@ -2,7 +2,6 @@ import functools
 import keras_applications as ka
 
 from .models import resnet as rn
-from .models import resnet_a as rn_a
 from .models import resnext as rx
 from .models import senet as sn
 
@@ -18,27 +17,16 @@ class ModelsFactory:
         'resnet152': [rn.ResNet152, rn.preprocess_input],
         
         # ResNets with Front Augmented 
-        'resnet18faa' : [rn_a.ResNet18FAA, rn_a.preprocess_input],
-        'resnet34faa' : [rn_a.ResNet34FAA, rn_a.preprocess_input],
+        'resnet18faa' : [rn.ResNet18FAA, rn_a.preprocess_input],
+        'resnet34faa' : [rn.ResNet34FAA, rn_a.preprocess_input],
         
         # ResNets with Back Augmented
-        'resnet18baa' : [rn_a.ResNet18BAA, rn_a.preprocess_input],
-        'resnet34baa' : [rn_a.ResNet34BAA, rn_a.preprocess_input],
+        'resnet18baa' : [rn.ResNet18BAA, rn_a.preprocess_input],
+        'resnet34baa' : [rn.ResNet34BAA, rn_a.preprocess_input],
         
         # ResNets with Full Augmented
-        'resnet18aa_test' : [rn_a.ResNet18AA, rn_a.preprocess_input],
-        'resnet34aa_test' : [rn_a.ResNet34AA, rn_a.preprocess_input],
-        'resnet50aa_test' : [rn_a.ResNet50AA, rn_a.preprocess_input],  
-        
-        # ResNets with SA
-        'resnet18sa': [rn.ResNet18SA, rn.preprocess_input],
-        'resnet34sa' : [rn.ResNet34SA, rn.preprocess_input],
-        'resnet50sa' : [rn.ResNet50SA, rn.preprocess_input],
-        
-        # ResNets with Attention
-        'resnet18sa' : [rn_a.ResNet18SA, rn_a.preprocess_input],
-        'resnet34sa' : [rn_a.ResNet34SA, rn_a.preprocess_input],
-        'resnet50sa' : [rn_a.ResNet50SA, rn_a.preprocess_input],
+        'resnet18aa_test' : [rn.ResNet18AA, rn_a.preprocess_input],
+        'resnet34aa_test' : [rn.ResNet34AA, rn_a.preprocess_input],
      
         # SE-Nets
         'seresnet18': [rn.SEResNet18, rn.preprocess_input],
