958b5a4e9547b8755781306b031c5fc59e23e3a2
JeremyOng96 <jeremyunderscore@gmail.com>
2020-11-18 15:10:35 +0800

    Update resnet.py

--- classification_models/models/resnet.py
+++ classification_models/models/resnet.py
@@ -147,7 +147,7 @@ def residual_augmented_conv_block(filters, stage, block, strides=(1, 1), attenti
         x = layers.Activation('relu', name=relu_name + '2')(x)
         
         # x = layers.ZeroPadding2D(padding=(1, 1))(x)
-        x = AugmentedConv2d(filters, (3,3), strides = 1)(x)
+        x = AugmentedConv2d(filters, (3,3), strides = (1,1))(x)
         # x = layers.Conv2D(filters, (3, 3), name=conv_name + '2', **conv_params)(x)
 
         # use attention block if defined
