fa1979ef05bf3f67880b8b3bc724f28a2a5c3b90
JeremyOng96 <jeremyunderscore@gmail.com>
2020-11-18 15:01:44 +0800

    Update resnet_a.py

--- classification_models/models/resnet_a.py
+++ classification_models/models/resnet_a.py
@@ -115,7 +115,7 @@ def p_front_augmented_residual_conv_block(filters, stage, block, strides=(1, 1),
 
     return layer
 
-    def p_back_residual_augmented_conv_block(filters, stage, block, strides=(1, 1), attention=None, cut='pre'):
+def p_back_residual_augmented_conv_block(filters, stage, block, strides=(1, 1), attention=None, cut='pre'):
     """The identity block is the block that has no conv layer at shortcut.
     # Arguments
     input_tensor: input tensor
@@ -203,62 +203,6 @@ def augmented_residual_conv_block(filters, stage, block, strides=(1, 1), attenti
         x = layers.BatchNormalization(name=bn_name + '2', **bn_params)(x)
         x = layers.Activation('relu', name=relu_name + '2')(x)
 
-        x = AugmentedConv2d(filters, (3,3), strides = 1)(x)
-
-        # use attention block if defined
-        if attention is not None:
-            x = attention(x)
-
-        # add residual connection
-        x = layers.Add()([x, shortcut])
-        return x
-
-    return layer
-
-def augmented_residual_conv_block(filters, stage, block, strides=(1, 1), attention=None, cut='pre'):
-    """Augmented residual conv block which has conv layer at shortcut when stride = 2.
-    # Arguments
-    input_tensor: input tensor
-    kernel_size: default 3, the kernel size of
-        middle conv layer at main path
-    filters: list of integers, the filters of 3 conv layer at main path
-    stage: integer, current stage label, used for generating layer names
-    block: 'a','b'..., current block label, used for generating layer names
-    cut: one of 'pre', 'post'. used to decide where skip connection is taken
-    all_attn: If True all conv blocks are replaced with augmented convolution. Else, only the last block is augmented conv
-    # Returns
-    Output tensor for the block.
-    """
-
-    def layer(input_tensor):
-
-        # get params and names of layers
-        conv_params = get_conv_params()
-        bn_params = get_bn_params()
-        conv_name, bn_name, relu_name, sc_name = handle_block_names(stage, block)
-
-        x = layers.BatchNormalization(name=bn_name + '1', **bn_params)(input_tensor)
-        x = layers.Activation('relu', name=relu_name + '1')(x)
-
-        # defining shortcut connection
-        if cut == 'pre':
-            shortcut = input_tensor
-        elif cut == 'post':
-            shortcut = layers.Conv2D(filters, (1, 1), name=sc_name, strides=strides, **conv_params)(x)
-        else:
-            raise ValueError('Cut type not in ["pre", "post"]')
-
-        # continue with convolution layers
-        if all_attn:
-            x = AugmentedConv2d(filters, (3,3), strides = strides)(x)
-        else:
-            x = layers.ZeroPadding2D(padding=(1, 1))(x)
-            x = layers.Conv2D(filters, (3, 3), strides=strides, name=conv_name + '1', **conv_params)(x)
-
-        x = layers.BatchNormalization(name=bn_name + '2', **bn_params)(x)
-        x = layers.Activation('relu', name=relu_name + '2')(x)          
-
-        # applies the final augmented convolution
         x = AugmentedConv2d(filters, (3,3), strides = 1)(x)
 
         # use attention block if defined
