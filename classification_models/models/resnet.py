cc8522bd2e9acbe4f09da68afe24c73b3af1449f
JeremyOng96 <jeremyunderscore@gmail.com>
2020-11-25 13:35:48 +0800

    Update resnet.py

--- classification_models/models/resnet.py
+++ classification_models/models/resnet.py
@@ -143,8 +143,8 @@ def augmented_residual_conv_block(filters, stage, block, strides=(1, 1), attenti
         # continue with convolution layers
         if str(stage+1) in '234' and str(block+1) in '123456':
             if strides != (1,1):
-                #x = layers.Conv2D(filters,(1,1), strides = strides)(x) # Downsampling at block 1 
-                x = layers.AveragePooling2D()(x)
+                x = layers.Conv2D(filters,(1,1), strides = strides)(x) # Downsampling at block 1 
+                
             x = augmented_conv2d(x, filters = filters, kernel_size = (3,3), depth_k = 0.25, depth_v = 0.25, num_heads = 8, relative_encodings = True)
 
             x = layers.Activation('relu', name=relu_name + '2')(x)      
