67927ca71cff6257b66284cd700f2db2a8548a6f
JeremyOng96 <jeremyunderscore@gmail.com>
2020-11-26 15:50:43 +0800

    Update resnet.py

--- classification_models/models/resnet.py
+++ classification_models/models/resnet.py
@@ -2,7 +2,7 @@ import os
 import collections
 
 
-from ._common_blocks import ChannelSE, cbam_block
+from ._common_blocks import ChannelSE, cbam_block, AugmentedConv2D
 from .. import get_submodules_from_kwargs
 from ..weights import load_model_weights
 
@@ -97,6 +97,57 @@ def residual_conv_block(filters, stage, block, strides=(1, 1), attention=None, c
         
         x = layers.ZeroPadding2D(padding=(1, 1))(x)
         x = layers.Conv2D(filters, (3, 3), name=conv_name + '2', **conv_params)(x)
+
+        # use attention block if defined
+        if attention is not None:
+            x = attention(x)
+
+        # add residual connection
+        x = layers.Add()([x, shortcut])
+        return x
+
+    return layer
+
+def augmented_residual_conv_block(filters, stage, block, strides=(1, 1), attention=None, cut='pre'):
+    """The identity block is the block that has no conv layer at shortcut.
+    # Arguments
+        input_tensor: input tensor
+        kernel_size: default 3, the kernel size of
+            middle conv layer at main path
+        filters: list of integers, the filters of 3 conv layer at main path
+        stage: integer, current stage label, used for generating layer names
+        block: 'a','b'..., current block label, used for generating layer names
+        cut: one of 'pre', 'post'. used to decide where skip connection is taken
+    # Returns
+        Output tensor for the block.
+    """
+
+    def layer(input_tensor):
+
+        # get params and names of layers
+        conv_params = get_conv_params()
+        bn_params = get_bn_params()
+        conv_name, bn_name, relu_name, sc_name = handle_block_names(stage, block)
+
+        x = layers.BatchNormalization(name=bn_name + '1', **bn_params)(input_tensor)
+        x = layers.Activation('relu', name=relu_name + '1')(x)
+
+        # defining shortcut connection
+        if cut == 'pre':
+            shortcut = input_tensor
+        elif cut == 'post':
+            shortcut = layers.Conv2D(filters, (1, 1), name=sc_name, strides=strides, **conv_params)(x)
+        else:
+            raise ValueError('Cut type not in ["pre", "post"]')
+
+        # continue with convolution layers
+        x = layers.ZeroPadding2D(padding=(1, 1))(x)
+        x = layers.Conv2D(filters, (3, 3), strides=strides, name=conv_name + '1', **conv_params)(x)
+        x = layers.BatchNormalization(name=bn_name + '2', **bn_params)(x)
+        x = layers.Activation('relu', name=relu_name + '2')(x)
+        
+        if str(stage+1) in "234":
+            x = AugmentedConv2D(filters, (3,3), stage=stage+1,block=block+1)(x)
 
         # use attention block if defined
         if attention is not None:
